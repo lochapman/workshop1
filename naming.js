fd0cd2bf0d0f461d466ce83af2da66e8410b2816
Lukas Chapman <lukas.chapman@computingforall.org>
2020-07-07 14:51:02 -0700

    constants and variables

---
+++ naming.js
@@ -0,0 +1,11 @@
+var firstNum = 14;
+var secNum = 15;
+var sanNum = 1.6;
+var quatroNum = .17;
+//beginning constants
+const cincoDe = 18;
+const seisY = .019;
+const dreiNum = 0.02;
+const veiNum = 0.03;
+const einesNum = 0.006;
+console.log ('firstNum', 'secNum', 'sanNum', 'quatroNum', 'cincoDe','seisY','dreiNum','veiNum','einesNum' )
